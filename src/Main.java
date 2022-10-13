08402671c3c1ff184ce947e81a663b9a28eec72e
andrealves8 <andrealves-tec@hotmail.com>
2022-10-13 09:53:38 -0300

    classe Curso e Mentoria

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Java");
+        curso1.setDescricao("POO");
+        curso1.setCargaHoraria(6);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Java");
+        curso2.setDescricao("Spring");
+        curso2.setCargaHoraria(3);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Banco de Dados");
+        mentoria.setDescricao("SQL");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
